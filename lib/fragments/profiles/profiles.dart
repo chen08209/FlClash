0c5314bddd5d1553d2c466ca827e8db4b0a9710a
Ember Moth <ember_moth@proton.me>
2025-02-21 17:21:22 +0800

    Update profiles.dart

--- lib/fragments/profiles/profiles.dart
+++ lib/fragments/profiles/profiles.dart
@@ -1,5 +1,5 @@
-// lib/fragments/profiles/profiles.dart
 import 'dart:ui';
+
 import 'package:fl_clash/common/common.dart';
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/fragments/profiles/edit_profile.dart';
@@ -9,6 +9,8 @@ import 'package:fl_clash/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+
+import 'add_profile.dart';
 
 class ProfilesFragment extends StatefulWidget {
   const ProfilesFragment({super.key});
@@ -20,76 +22,91 @@ class ProfilesFragment extends StatefulWidget {
 class _ProfilesFragmentState extends State<ProfilesFragment> {
   Function? applyConfigDebounce;
 
-  @override
-  void initState() {
-    super.initState();
-    _checkAndAddSubscribeProfile();
+  _handleShowAddExtendPage() {
+    showExtendPage(
+      globalState.navigatorKey.currentState!.context,
+      body: AddProfile(
+        context: globalState.navigatorKey.currentState!.context,
+      ),
+      title: "${appLocalizations.add}${appLocalizations.profile}",
+    );
   }
 
-  Future<void> _checkAndAddSubscribeProfile() async {
-    final config = Provider.of<Config>(context, listen: false);
-    if (config.subscribeUrl != null && config.profiles.isEmpty) {
-      final newProfile = Profile.normal(label: "订阅配置", url: config.subscribeUrl!);
-      config.setProfile(newProfile);
-      await newProfile.checkAndUpdate();
-    }
-  }
-
-  Future<void> _updateProfiles() async {
+  _updateProfiles() async {
     final appController = globalState.appController;
     final config = appController.config;
     final profiles = appController.config.profiles;
     final messages = [];
-    final updateProfiles = profiles.map<Future>((profile) async {
-      if (profile.type == ProfileType.file) return;
-      config.setProfile(profile.copyWith(isUpdating: true));
-      try {
-        await appController.updateProfile(profile);
-      } catch (e) {
-        messages.add("${profile.label ?? profile.id}: $e \n");
-        config.setProfile(profile.copyWith(isUpdating: false));
-      }
-    });
+    final updateProfiles = profiles.map<Future>(
+      (profile) async {
+        if (profile.type == ProfileType.file) return;
+        config.setProfile(
+          profile.copyWith(isUpdating: true),
+        );
+        try {
+          await appController.updateProfile(profile);
+        } catch (e) {
+          messages.add("${profile.label ?? profile.id}: $e \n");
+          config.setProfile(
+            profile.copyWith(
+              isUpdating: false,
+            ),
+          );
+        }
+      },
+    );
     final titleMedium = context.textTheme.titleMedium;
     await Future.wait(updateProfiles);
     if (messages.isNotEmpty) {
       globalState.showMessage(
         title: appLocalizations.tip,
         message: TextSpan(
-          children: [for (final message in messages) TextSpan(text: message, style: titleMedium)],
+          children: [
+            for (final message in messages)
+              TextSpan(text: message, style: titleMedium)
+          ],
         ),
       );
     }
   }
 
-  void _initScaffold() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) return;
-      final commonScaffoldState = context.commonScaffoldState;
-      commonScaffoldState?.actions = [
-        IconButton(
-          onPressed: () {
-            _updateProfiles();
-          },
-          icon: const Icon(Icons.sync),
-        ),
-        IconButton(
-          onPressed: () {
-            final profiles = globalState.appController.config.profiles;
-            showSheet(
-              title: appLocalizations.profilesSort,
-              context: context,
-              body: SizedBox(
-                height: 400,
-                child: ReorderableProfiles(profiles: profiles),
-              ),
-            );
-          },
-          icon: const Icon(Icons.sort),
-          iconSize: 26,
-        ),
-      ];
-    });
+  _initScaffold() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        if (!mounted) return;
+        final commonScaffoldState = context.commonScaffoldState;
+        commonScaffoldState?.actions = [
+          IconButton(
+            onPressed: () {
+              _updateProfiles();
+            },
+            icon: const Icon(Icons.sync),
+          ),
+          IconButton(
+            onPressed: () {
+              final profiles = globalState.appController.config.profiles;
+              showSheet(
+                title: appLocalizations.profilesSort,
+                context: context,
+                body: SizedBox(
+                  height: 400,
+                  child: ReorderableProfiles(profiles: profiles),
+                ),
+              );
+            },
+            icon: const Icon(Icons.sort),
+            iconSize: 26,
+          ),
+        ];
+        commonScaffoldState?.floatingActionButton = FloatingActionButton(
+          heroTag: null,
+          onPressed: _handleShowAddExtendPage,
+          child: const Icon(
+            Icons.add,
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -110,14 +127,19 @@ class _ProfilesFragmentState extends State<ProfilesFragment> {
         ),
         builder: (context, state, child) {
           if (state.profiles.isEmpty) {
-            return const NullStatus(
-              label: "暂无配置文件",
+            return NullStatus(
+              label: appLocalizations.nullProfileDesc,
             );
           }
           return Align(
             alignment: Alignment.topCenter,
             child: SingleChildScrollView(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 88),
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                top: 16,
+                bottom: 88,
+              ),
               child: Grid(
                 mainAxisSpacing: 16,
                 crossAxisSpacing: 16,
@@ -157,9 +179,13 @@ class ProfileItem extends StatelessWidget {
   _handleDeleteProfile(BuildContext context) async {
     final res = await globalState.showMessage(
       title: appLocalizations.tip,
-      message: const TextSpan(text: "确定要删除此配置文件吗？"),
+      message: TextSpan(
+        text: appLocalizations.deleteProfileTip,
+      ),
     );
-    if (res != true) return;
+    if (res != true) {
+      return;
+    }
     await globalState.appController.deleteProfile(profile.id);
   }
 
@@ -167,23 +193,36 @@ class ProfileItem extends StatelessWidget {
     await globalState.safeRun<void>(updateProfile);
   }
 
-  Future<void> updateProfile() async {
+  Future updateProfile() async {
     final appController = globalState.appController;
     final config = appController.config;
     if (profile.type == ProfileType.file) return;
-    config.setProfile(profile.copyWith(isUpdating: true));
-    try {
-      await appController.updateProfile(profile);
-    } catch (e) {
-      config.setProfile(profile.copyWith(isUpdating: false));
-      rethrow;
-    }
+    await globalState.safeRun(silence: false, () async {
+      try {
+        config.setProfile(
+          profile.copyWith(
+            isUpdating: true,
+          ),
+        );
+        await appController.updateProfile(profile);
+      } catch (e) {
+        config.setProfile(
+          profile.copyWith(
+            isUpdating: false,
+          ),
+        );
+        rethrow;
+      }
+    });
   }
 
   _handleShowEditExtendPage(BuildContext context) {
     showExtendPage(
       context,
-      body: EditProfile(profile: profile, context: context),
+      body: EditProfile(
+        profile: profile,
+        context: context,
+      ),
       title: "${appLocalizations.edit}${appLocalizations.profile}",
     );
   }
@@ -191,9 +230,13 @@ class ProfileItem extends StatelessWidget {
   List<Widget> _buildUrlProfileInfo(BuildContext context) {
     final subscriptionInfo = profile.subscriptionInfo;
     return [
-      const SizedBox(height: 8),
+      const SizedBox(
+        height: 8,
+      ),
       if (subscriptionInfo != null)
-        SubscriptionInfoView(subscriptionInfo: subscriptionInfo),
+        SubscriptionInfoView(
+          subscriptionInfo: subscriptionInfo,
+        ),
       Text(
         profile.lastUpdateDate?.lastUpdateTimeDesc ?? "",
         style: context.textTheme.labelMedium?.toLight,
@@ -203,7 +246,9 @@ class ProfileItem extends StatelessWidget {
 
   List<Widget> _buildFileProfileInfo(BuildContext context) {
     return [
-      const SizedBox(height: 8),
+      const SizedBox(
+        height: 8,
+      ),
       Text(
         profile.lastUpdateDate?.lastUpdateTimeDesc ?? "",
         style: context.textTheme.labelMedium?.toLight,
@@ -212,7 +257,11 @@ class ProfileItem extends StatelessWidget {
   }
 
   _handleCopyLink(BuildContext context) async {
-    await Clipboard.setData(ClipboardData(text: profile.url));
+    await Clipboard.setData(
+      ClipboardData(
+        text: profile.url,
+      ),
+    );
     if (context.mounted) {
       context.showNotifier(appLocalizations.copySuccess);
     }
@@ -220,11 +269,18 @@ class ProfileItem extends StatelessWidget {
 
   _handleExportFile(BuildContext context) async {
     final commonScaffoldState = context.commonScaffoldState;
-    final res = await commonScaffoldState?.loadingRun<bool>(() async {
-      final file = await profile.getFile();
-      final value = await picker.saveFile(profile.label ?? profile.id, file.readAsBytesSync());
-      return value != null;
-    }, title: appLocalizations.tip);
+    final res = await commonScaffoldState?.loadingRun<bool>(
+      () async {
+        final file = await profile.getFile();
+        final value = await picker.saveFile(
+          profile.label ?? profile.id,
+          file.readAsBytesSync(),
+        );
+        if (value == null) return false;
+        return true;
+      },
+      title: appLocalizations.tip,
+    );
     if (res == true && context.mounted) {
       context.showNotifier(appLocalizations.exportSuccess);
     }
@@ -235,7 +291,9 @@ class ProfileItem extends StatelessWidget {
     final key = GlobalKey<CommonPopupBoxState>();
     return CommonCard(
       isSelected: profile.id == groupValue,
-      onPressed: () => onChanged(profile.id),
+      onPressed: () {
+        onChanged(profile.id);
+      },
       child: ListItem(
         key: Key(profile.id),
         horizontalTitleGap: 16,
@@ -256,37 +314,49 @@ class ProfileItem extends StatelessWidget {
                         ActionItemData(
                           icon: Icons.edit_outlined,
                           label: appLocalizations.edit,
-                          onPressed: () => _handleShowEditExtendPage(context),
+                          onPressed: () {
+                            _handleShowEditExtendPage(context);
+                          },
                         ),
                         if (profile.type == ProfileType.url) ...[
                           ActionItemData(
                             icon: Icons.sync_alt_sharp,
                             label: appLocalizations.sync,
-                            onPressed: _handleUpdateProfile,
+                            onPressed: () {
+                              _handleUpdateProfile();
+                            },
                           ),
                           ActionItemData(
                             icon: Icons.copy,
                             label: appLocalizations.copyLink,
-                            onPressed: () => _handleCopyLink(context),
+                            onPressed: () {
+                              _handleCopyLink(context);
+                            },
                           ),
                         ],
                         ActionItemData(
                           icon: Icons.file_copy_outlined,
                           label: appLocalizations.exportFile,
-                          onPressed: () => _handleExportFile(context),
+                          onPressed: () {
+                            _handleExportFile(context);
+                          },
                         ),
                         ActionItemData(
                           icon: Icons.delete_outlined,
                           iconSize: 20,
                           label: appLocalizations.delete,
-                          onPressed: () => _handleDeleteProfile(context),
+                          onPressed: () {
+                            _handleDeleteProfile(context);
+                          },
                           type: ActionType.danger,
                         ),
                       ],
                     ),
                     target: IconButton(
-                      onPressed: () => key.currentState?.pop(),
-                      icon: const Icon(Icons.more_vert),
+                      onPressed: () {
+                        key.currentState?.pop();
+                      },
+                      icon: Icon(Icons.more_vert),
                     ),
                   ),
           ),
@@ -306,10 +376,13 @@ class ProfileItem extends StatelessWidget {
               Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: switch (profile.type) {
-                  ProfileType.file => _buildFileProfileInfo(context),
-                  ProfileType.url => _buildUrlProfileInfo(context),
-                },
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ...switch (profile.type) {
+                    ProfileType.file => _buildFileProfileInfo(context),
+                    ProfileType.url => _buildUrlProfileInfo(context),
+                  },
+                ],
               ),
             ],
           ),
@@ -323,7 +396,10 @@ class ProfileItem extends StatelessWidget {
 class ReorderableProfiles extends StatefulWidget {
   final List<Profile> profiles;
 
-  const ReorderableProfiles({super.key, required this.profiles});
+  const ReorderableProfiles({
+    super.key,
+    required this.profiles,
+  });
 
   @override
   State<ReorderableProfiles> createState() => _ReorderableProfilesState();
@@ -338,14 +414,21 @@ class _ReorderableProfilesState extends State<ReorderableProfiles> {
     profiles = List.from(widget.profiles);
   }
 
-  Widget proxyDecorator(Widget child, int index, Animation<double> animation) {
+  Widget proxyDecorator(
+    Widget child,
+    int index,
+    Animation<double> animation,
+  ) {
     final profile = profiles[index];
     return AnimatedBuilder(
       animation: animation,
       builder: (_, Widget? child) {
         final double animValue = Curves.easeInOut.transform(animation.value);
         final double scale = lerpDouble(1, 1.02, animValue)!;
-        return Transform.scale(scale: scale, child: child);
+        return Transform.scale(
+          scale: scale,
+          child: child,
+        );
       },
       child: Container(
         key: Key(profile.id),
@@ -353,7 +436,10 @@ class _ReorderableProfilesState extends State<ReorderableProfiles> {
         child: CommonCard(
           type: CommonCardType.filled,
           child: ListTile(
-            contentPadding: const EdgeInsets.only(right: 44, left: 16),
+            contentPadding: const EdgeInsets.only(
+              right: 44,
+              left: 16,
+            ),
             title: Text(profile.label ?? profile.id),
           ),
         ),
@@ -374,7 +460,9 @@ class _ReorderableProfilesState extends State<ReorderableProfiles> {
             proxyDecorator: proxyDecorator,
             onReorder: (oldIndex, newIndex) {
               setState(() {
-                if (oldIndex < newIndex) newIndex -= 1;
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
                 final profile = profiles.removeAt(oldIndex);
                 profiles.insert(newIndex, profile);
               });
@@ -387,7 +475,10 @@ class _ReorderableProfilesState extends State<ReorderableProfiles> {
                 child: CommonCard(
                   type: CommonCardType.filled,
                   child: ListTile(
-                    contentPadding: const EdgeInsets.only(right: 16, left: 16),
+                    contentPadding: const EdgeInsets.only(
+                      right: 16,
+                      left: 16,
+                    ),
                     title: Text(profile.label ?? profile.id),
                     trailing: ReorderableDragStartListener(
                       index: index,
@@ -401,18 +492,27 @@ class _ReorderableProfilesState extends State<ReorderableProfiles> {
           ),
         ),
         Container(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+          padding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 24,
+          ),
           child: FilledButton.tonal(
             onPressed: () {
               Navigator.of(context).pop();
               globalState.appController.config.profiles = profiles;
             },
             style: ButtonStyle(
-              padding: WidgetStateProperty.all(const EdgeInsets.symmetric(vertical: 8)),
+              padding: WidgetStateProperty.all(
+                const EdgeInsets.symmetric(vertical: 8),
+              ),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text(appLocalizations.confirm)],
+              children: [
+                Text(
+                  appLocalizations.confirm,
+                ),
+              ],
             ),
           ),
         ),
