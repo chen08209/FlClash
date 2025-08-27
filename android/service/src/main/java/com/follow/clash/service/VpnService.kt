Lochinemak/YuClash

Add android separates the core process
Support core status check and force restart

Update flutter and pub dependencies

android/service/src/main/java/com/follow/clash/service/VpnService.kt
@@ -1,0 +1,249 @@
+package com.follow.clash.service
+
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.ProxyInfo
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.os.Parcel
+import android.os.RemoteException
+import android.util.Log
+import androidx.core.content.getSystemService
+import com.follow.clash.common.AccessControlMode
+import com.follow.clash.common.QuickAction
+import com.follow.clash.common.quickIntent
+import com.follow.clash.common.toPendingIntent
+import com.follow.clash.core.Core
+import com.follow.clash.service.models.VpnOptions
+import com.follow.clash.service.models.getIpv4RouteAddress
+import com.follow.clash.service.models.getIpv6RouteAddress
+import com.follow.clash.service.models.toCIDR
+import com.follow.clash.service.modules.NetworkObserveModule
+import com.follow.clash.service.modules.NotificationModule
+import com.follow.clash.service.modules.SuspendModule
+import com.follow.clash.service.modules.moduleLoader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import java.net.InetSocketAddress
+import android.net.VpnService as SystemVpnService
+
+class VpnService : SystemVpnService(), IBaseService,
+    CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+    private val self: VpnService
+        get() = this
+
+    private val loader = moduleLoader {
+        install(NetworkObserveModule(self))
+        install(NotificationModule(self))
+        install(SuspendModule(self))
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        handleCreate()
+    }
+    private val connectivity by lazy {
+        getSystemService<ConnectivityManager>()
+    }
+    private val uidPageNameMap = mutableMapOf<Int, String>()
+
+    private fun resolverProcess(
+        protocol: Int,
+        source: InetSocketAddress,
+        target: InetSocketAddress,
+        uid: Int,
+    ): String {
+        val nextUid = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            connectivity?.getConnectionOwnerUid(protocol, source, target) ?: -1
+        } else {
+            uid
+        }
+        if (nextUid == -1) {
+            return ""
+        }
+        if (!uidPageNameMap.containsKey(nextUid)) {
+            uidPageNameMap[nextUid] = this.packageManager?.getPackagesForUid(nextUid)?.first() ?: ""
+        }
+        return uidPageNameMap[nextUid] ?: ""
+    }
+
+    val VpnOptions.address
+        get(): String = buildString {
+            append(IPV4_ADDRESS)
+            if (ipv6) {
+                append(",")
+                append(IPV6_ADDRESS)
+            }
+        }
+
+    val VpnOptions.dns
+        get(): String {
+            if (dnsHijacking) {
+                return NET_ANY
+            }
+            return buildString {
+                append(DNS)
+                if (ipv6) {
+                    append(",")
+                    append(DNS6)
+                }
+            }
+        }
+
+
+    override fun onLowMemory() {
+        Core.forceGC()
+        super.onLowMemory()
+    }
+
+    private val binder = LocalBinder()
+
+    inner class LocalBinder : Binder() {
+        fun getService(): VpnService = this@VpnService
+
+        override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+            try {
+                val isSuccess = super.onTransact(code, data, reply, flags)
+                if (!isSuccess) {
+                    QuickAction.STOP.quickIntent.toPendingIntent.send()
+                }
+                return isSuccess
+            } catch (e: RemoteException) {
+                throw e
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    private fun handleStart(options: VpnOptions) {
+        val fd = with(Builder()) {
+            val cidr = IPV4_ADDRESS.toCIDR()
+            addAddress(cidr.address, cidr.prefixLength)
+            Log.d(
+                "addAddress", "address: ${cidr.address} prefixLength:${cidr.prefixLength}"
+            )
+            val routeAddress = options.getIpv4RouteAddress()
+            if (routeAddress.isNotEmpty()) {
+                try {
+                    routeAddress.forEach { i ->
+                        Log.d(
+                            "addRoute4", "address: ${i.address} prefixLength:${i.prefixLength}"
+                        )
+                        addRoute(i.address, i.prefixLength)
+                    }
+                } catch (_: Exception) {
+                    addRoute(NET_ANY, 0)
+                }
+            } else {
+                addRoute(NET_ANY, 0)
+            }
+            if (options.ipv6) {
+                try {
+                    val cidr = IPV6_ADDRESS.toCIDR()
+                    Log.d(
+                        "addAddress6", "address: ${cidr.address} prefixLength:${cidr.prefixLength}"
+                    )
+                    addAddress(cidr.address, cidr.prefixLength)
+                } catch (_: Exception) {
+                    Log.d(
+                        "addAddress6", "IPv6 is not supported."
+                    )
+                }
+
+                try {
+                    val routeAddress = options.getIpv6RouteAddress()
+                    if (routeAddress.isNotEmpty()) {
+                        try {
+                            routeAddress.forEach { i ->
+                                Log.d(
+                                    "addRoute6",
+                                    "address: ${i.address} prefixLength:${i.prefixLength}"
+                                )
+                                addRoute(i.address, i.prefixLength)
+                            }
+                        } catch (_: Exception) {
+                            addRoute("::", 0)
+                        }
+                    } else {
+                        addRoute(NET_ANY6, 0)
+                    }
+                } catch (_: Exception) {
+                    addRoute(NET_ANY6, 0)
+                }
+            }
+            addDnsServer(DNS)
+            if (options.ipv6) {
+                addDnsServer(DNS6)
+            }
+            setMtu(9000)
+            options.accessControl.let { accessControl ->
+                if (accessControl.enable) {
+                    when (accessControl.mode) {
+                        AccessControlMode.ACCEPT_SELECTED -> {
+                            (accessControl.acceptList + packageName).forEach {
+                                addAllowedApplication(it)
+                            }
+                        }
+
+                        AccessControlMode.REJECT_SELECTED -> {
+                            (accessControl.rejectList - packageName).forEach {
+                                addDisallowedApplication(it)
+                            }
+                        }
+                    }
+                }
+            }
+            setSession("FlClash")
+            setBlocking(false)
+            if (Build.VERSION.SDK_INT >= 29) {
+                setMetered(false)
+            }
+            if (options.allowBypass) {
+                allowBypass()
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && options.systemProxy) {
+                setHttpProxy(
+                    ProxyInfo.buildDirectProxy(
+                        "127.0.0.1", options.port, options.bypassDomain
+                    )
+                )
+            }
+            establish()?.detachFd()
+                ?: throw NullPointerException("Establish VPN rejected by system")
+        }
+        Core.startTun(
+            fd,
+            protect = this::protect,
+            resolverProcess = this::resolverProcess,
+            options.address,
+            options.dns
+        )
+    }
+
+    override fun start() {
+        loader.load()
+        State.options?.let {
+            handleStart(it)
+        }
+    }
+
+    override fun stop() {
+        loader.cancel()
+        Core.stopTun()
+        stopSelf()
+    }
+
+    companion object {
+        private const val IPV4_ADDRESS = "172.19.0.1/30"
+        private const val IPV6_ADDRESS = "fdfe:dcba:9876::1/126"
+        private const val DNS = "172.19.0.2"
+        private const val DNS6 = "fdfe:dcba:9876::2"
+        private const val NET_ANY = "0.0.0.0"
+        private const val NET_ANY6 = "::"
+    }
+}

chen08209/FlClash

android/service/src/main/java/com/follow/clash/service/VpnService.kt
@@ -1,0 +1,251 @@
+package com.follow.clash.service
+
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.ProxyInfo
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.os.Parcel
+import android.os.RemoteException
+import android.util.Log
+import androidx.core.content.getSystemService
+import com.follow.clash.common.AccessControlMode
+import com.follow.clash.common.BroadcastAction
+import com.follow.clash.common.GlobalState
+import com.follow.clash.common.sendBroadcast
+import com.follow.clash.core.Core
+import com.follow.clash.service.models.VpnOptions
+import com.follow.clash.service.models.getIpv4RouteAddress
+import com.follow.clash.service.models.getIpv6RouteAddress
+import com.follow.clash.service.models.toCIDR
+import com.follow.clash.service.modules.NetworkObserveModule
+import com.follow.clash.service.modules.NotificationModule
+import com.follow.clash.service.modules.SuspendModule
+import com.follow.clash.service.modules.moduleLoader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import java.net.InetSocketAddress
+import android.net.VpnService as SystemVpnService
+
+class VpnService : SystemVpnService(), IBaseService,
+    CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+    private val self: VpnService
+        get() = this
+
+    private val loader = moduleLoader {
+        install(NetworkObserveModule(self))
+        install(NotificationModule(self))
+        install(SuspendModule(self))
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        handleCreate()
+    }
+
+    private val connectivity by lazy {
+        getSystemService<ConnectivityManager>()
+    }
+    private val uidPageNameMap = mutableMapOf<Int, String>()
+
+    private fun resolverProcess(
+        protocol: Int,
+        source: InetSocketAddress,
+        target: InetSocketAddress,
+        uid: Int,
+    ): String {
+        val nextUid = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            connectivity?.getConnectionOwnerUid(protocol, source, target) ?: -1
+        } else {
+            uid
+        }
+        if (nextUid == -1) {
+            return ""
+        }
+        if (!uidPageNameMap.containsKey(nextUid)) {
+            uidPageNameMap[nextUid] = this.packageManager?.getPackagesForUid(nextUid)?.first() ?: ""
+        }
+        return uidPageNameMap[nextUid] ?: ""
+    }
+
+    val VpnOptions.address
+        get(): String = buildString {
+            append(IPV4_ADDRESS)
+            if (ipv6) {
+                append(",")
+                append(IPV6_ADDRESS)
+            }
+        }
+
+    val VpnOptions.dns
+        get(): String {
+            if (dnsHijacking) {
+                return NET_ANY
+            }
+            return buildString {
+                append(DNS)
+                if (ipv6) {
+                    append(",")
+                    append(DNS6)
+                }
+            }
+        }
+
+
+    override fun onLowMemory() {
+        Core.forceGC()
+        super.onLowMemory()
+    }
+
+    private val binder = LocalBinder()
+
+    inner class LocalBinder : Binder() {
+        fun getService(): VpnService = this@VpnService
+
+        override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+            try {
+                val isSuccess = super.onTransact(code, data, reply, flags)
+                if (!isSuccess) {
+                    GlobalState.log("onTransact error ===>")
+                    BroadcastAction.STOP.sendBroadcast()
+                }
+                return isSuccess
+            } catch (e: RemoteException) {
+                throw e
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    private fun handleStart(options: VpnOptions) {
+        val fd = with(Builder()) {
+            val cidr = IPV4_ADDRESS.toCIDR()
+            addAddress(cidr.address, cidr.prefixLength)
+            Log.d(
+                "addAddress", "address: ${cidr.address} prefixLength:${cidr.prefixLength}"
+            )
+            val routeAddress = options.getIpv4RouteAddress()
+            if (routeAddress.isNotEmpty()) {
+                try {
+                    routeAddress.forEach { i ->
+                        Log.d(
+                            "addRoute4", "address: ${i.address} prefixLength:${i.prefixLength}"
+                        )
+                        addRoute(i.address, i.prefixLength)
+                    }
+                } catch (_: Exception) {
+                    addRoute(NET_ANY, 0)
+                }
+            } else {
+                addRoute(NET_ANY, 0)
+            }
+            if (options.ipv6) {
+                try {
+                    val cidr = IPV6_ADDRESS.toCIDR()
+                    Log.d(
+                        "addAddress6", "address: ${cidr.address} prefixLength:${cidr.prefixLength}"
+                    )
+                    addAddress(cidr.address, cidr.prefixLength)
+                } catch (_: Exception) {
+                    Log.d(
+                        "addAddress6", "IPv6 is not supported."
+                    )
+                }
+
+                try {
+                    val routeAddress = options.getIpv6RouteAddress()
+                    if (routeAddress.isNotEmpty()) {
+                        try {
+                            routeAddress.forEach { i ->
+                                Log.d(
+                                    "addRoute6",
+                                    "address: ${i.address} prefixLength:${i.prefixLength}"
+                                )
+                                addRoute(i.address, i.prefixLength)
+                            }
+                        } catch (_: Exception) {
+                            addRoute("::", 0)
+                        }
+                    } else {
+                        addRoute(NET_ANY6, 0)
+                    }
+                } catch (_: Exception) {
+                    addRoute(NET_ANY6, 0)
+                }
+            }
+            addDnsServer(DNS)
+            if (options.ipv6) {
+                addDnsServer(DNS6)
+            }
+            setMtu(9000)
+            options.accessControl.let { accessControl ->
+                if (accessControl.enable) {
+                    when (accessControl.mode) {
+                        AccessControlMode.ACCEPT_SELECTED -> {
+                            (accessControl.acceptList + packageName).forEach {
+                                addAllowedApplication(it)
+                            }
+                        }
+
+                        AccessControlMode.REJECT_SELECTED -> {
+                            (accessControl.rejectList - packageName).forEach {
+                                addDisallowedApplication(it)
+                            }
+                        }
+                    }
+                }
+            }
+            setSession("FlClash")
+            setBlocking(false)
+            if (Build.VERSION.SDK_INT >= 29) {
+                setMetered(false)
+            }
+            if (options.allowBypass) {
+                allowBypass()
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && options.systemProxy) {
+                setHttpProxy(
+                    ProxyInfo.buildDirectProxy(
+                        "127.0.0.1", options.port, options.bypassDomain
+                    )
+                )
+            }
+            establish()?.detachFd()
+                ?: throw NullPointerException("Establish VPN rejected by system")
+        }
+        Core.startTun(
+            fd,
+            protect = this::protect,
+            resolverProcess = this::resolverProcess,
+            options.address,
+            options.dns
+        )
+    }
+
+    override fun start() {
+        loader.load()
+        State.options?.let {
+            handleStart(it)
+        }
+    }
+
+    override fun stop() {
+        loader.cancel()
+        Core.stopTun()
+        stopSelf()
+    }
+
+    companion object {
+        private const val IPV4_ADDRESS = "172.19.0.1/30"
+        private const val IPV6_ADDRESS = "fdfe:dcba:9876::1/126"
+        private const val DNS = "172.19.0.2"
+        private const val DNS6 = "fdfe:dcba:9876::2"
+        private const val NET_ANY = "0.0.0.0"
+        private const val NET_ANY6 = "::"
+    }
+}