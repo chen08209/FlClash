48bf5f5306a9a7260f48db2c226bb4e9813ca232
chen08209 <chen08209@gmail.com>
2026-01-15 18:22:25 +0800

    cache

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -104,7 +104,6 @@ class HomePage extends StatelessWidget {
                     return view;
                   },
                 );
-                ;
               },
             ),
           ),
