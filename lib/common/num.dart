cad93d85c8e1af9b179276a0fcaf9453acde71c9
chen08209 <chen08209@gmail.com>
2026-08-22 06:03:11 +0800

    fix(common): tighten the moveOrEqual precision tolerance

--- lib/common/num.dart
+++ lib/common/num.dart
@@ -57,7 +57,7 @@ extension NumExt on num {
 
 extension DoubleExt on double {
   bool moreOrEqual(double value) {
-    return this > value || (value - this).abs() < precisionErrorTolerance + 1;
+    return this > value || (value - this).abs() < precisionErrorTolerance;
   }
 }
 
@@ -68,24 +68,5 @@ extension OffsetExt on Offset {
 
   double getMainAxisOffset(Axis direction) {
     return direction == Axis.vertical ? dy : dx;
-  }
-
-  bool less(Offset offset) {
-    if (dy < offset.dy) {
-      return true;
-    }
-    if (dy == offset.dy && dx < offset.dx) {
-      return true;
-    }
-    return false;
-  }
-}
-
-extension RectExt on Rect {
-  bool doRectIntersect(Rect rect) {
-    return left < rect.right &&
-        right > rect.left &&
-        top < rect.bottom &&
-        bottom > rect.top;
   }
 }
